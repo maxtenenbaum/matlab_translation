be99af6756042d06b1af664d7b9f66c56fd84570
cadapilla <133262385+cadapilla@users.noreply.github.com>
2024-02-26 23:04:18 -0800

    Delete matlab/getAllFiles.m

--- matlab/getAllFiles.m
+++
@@ -1,9 +0,0 @@
-function [filesList,numOfFiles] = getAllFiles(folderPath,ext)
-
-ext = erase(lower(ext),'.');
-ext_use = sprintf('*.%s',ext);
-filesInFolder = fullfile(folderPath,ext_use);    % files in folder
-filesList = dir(filesInFolder);               % add files to directory
-numOfFiles = length(filesList);               % number of .dta files
-
-end
