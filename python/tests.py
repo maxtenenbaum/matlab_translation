5376a126139dfaa6c2389f90d035331f9c156400
agentmistz <maxtenenbaum223@gmail.com>
2024-02-28 14:12:44 -0800

    read_bytes() implementation

--- python/tests.py
+++ python/tests.py
@@ -1,5 +1,23 @@
 import pyvisa
 import time
+"""
+This code currently works to connect to Keithley through RS-232 communication
+
+Working:
+Initialize resource manager
+List available resources
+Select and open resource
+
+Issues:
+I/O communication most likely due to problems with terminator
+6485 says line feed - "\n" is the terminator
+
+Currently implemented read_bytes() function in order to read out the actual bytes being read by computer
+- Last read byte should be the terminator
+"""
+
+
+
 
 # Initialize the VISA resource manager
 rm = pyvisa.ResourceManager()
@@ -16,15 +34,21 @@ resource_input = input("Which source would you like to use: ")
 resource_selection = source_options[int(resource_input)-1]
 
 # Open selected resource
-instance = rm.open_resource(resource_selection)
+instance = rm.open_resource(resource_selection, send_end=False, delay=1.2)
 
 # Initialize device
 print(instance)
-instance.read_termination = '\r' # MAY NEED TO CHANGE - ENSURE THAT DEVICE TERMINATOR IS SAME
-instance.write_termination = '\r' 
+"""instance.read_termination = '\n' # LINE FEED TERMINATOR
+instance.write_termination = '\n' 
 instance.baud_rate = 9600
-#instance.query_delay = 5
-instance.query("*IDN?")
+instance.query_delay = 5
+instance.write("*RST")
+instance.query("*IDN?")"""
+
+# Testing read_bytes() 
+instance.write('*IDN?')
+while True:
+    print(instance.read_bytes(1))
 
 
 
